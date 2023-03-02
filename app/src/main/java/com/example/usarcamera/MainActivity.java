e7c0b9a39009795b00ccb8d8cd670cc1e9a56cf3
Guilherme Scarlati <guilhermescarlati@gmail.com>
2023-03-02 19:33:54 -0300

    Commit da primeira sprint

---
+++ app/src/main/java/com/example/usarcamera/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.usarcamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
