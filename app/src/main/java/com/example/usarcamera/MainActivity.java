351dc819d9ce69d05d7cedba43a7009659171c1c
Guilherme Scarlati <guilhermescarlati@gmail.com>
2023-03-02 22:09:03 -0300

    Troquei o pedido de permissão e coloquei um código que é dado na documentação do android developer

--- app/src/main/java/com/example/usarcamera/MainActivity.java
+++ app/src/main/java/com/example/usarcamera/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap)  extras.get("data");
+                imageView.setImageBitmap(imageBitmap);
+            }
+        }
     }
 }
