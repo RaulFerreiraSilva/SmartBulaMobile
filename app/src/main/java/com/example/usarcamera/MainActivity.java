8ce7d00114e76fe6bb2acf9866a83653d228b61b
Guilherme Scarlati <guilhermescarlati@gmail.com>
2023-03-17 22:09:38 -0300

    Mudei algumas coisas no código e alinhei outras com o Raul, agora vai

--- app/src/main/java/com/example/usarcamera/MainActivity.java
+++ app/src/main/java/com/example/usarcamera/MainActivity.java
@@ -18,30 +18,33 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //private static final int PERMISSION_CODE = 1;
-    //private static final int CAPTURE_CODE = 1001;
     private AppCompatButton btFoto, enviarFoto;
     private ImageView fotinha;
-    private TextView tvTeste;
 
     //Uri img;
 
@@ -50,73 +53,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
-
         iniciarComponentes();
 
-        JsonObjectRequest obgJs = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    int userId = response.getInt("userId");
-                    int id = response.getInt("id");
-                    String title = response.getString("title");
-                    boolean completed = response.getBoolean("completed");
-
-                    tvTeste.setText(userId + "/n" + id + "/n" + title + "/n" + completed);
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                    tvTeste.setText("ERROR");
-            }
+        btFoto.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            resultFoto.launch(intent);
         });
 
-        if (!fotinha.equals(null)) {
-        fotinha.setVisibility(View.INVISIBLE);
-        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        requestQueue.add(obgJs);
-        }
-
-
-        btFoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                resultFoto.launch(intent);
-            }
-        });
-
-    }
-
-    //por enquanto vou tentar fazer um botão para enviar a foto para a API
-    //depois de enviar vou receber um resultado que no caso vai ser a bula abrindo
-    //posso fazer automaticamente colocando um handler talvez? Mas por enquanto fica assim
-    //vou jogar a bula em uma nova Activity ou Fragment, depende do que for melhor
-
-    //Aqui estou convertendo a foto em um array de bytes, porém como não vejo como aplicar isto agora
-    //vou deixar comentado para mais tarde puxar o código
-    /*public void enviarImg(View v, Bitmap img, String url){
-        Intent intent = new Intent();
-
-        ByteArrayOutputStream arrayBytes = new ByteArrayOutputStream();
-        img.compress(Bitmap.CompressFormat.PNG, 100,arrayBytes);
-        byte[] byteArray = arrayBytes.toByteArray();
-
-        //nesse starActivity, ele ja vai trazer a bula pra mim
-        startActivity(intent);
-    }*/
-
-    private void iniciarComponentes() {
-        fotinha = findViewById(R.id.fotoCamera);
-        btFoto = findViewById(R.id.fotobtn);
-        enviarFoto = findViewById(R.id.solicitar_resposta_btn);
-        tvTeste = findViewById(R.id.testAPI);
     }
 
     ActivityResultLauncher<Intent> resultFoto = registerForActivityResult(
@@ -125,47 +68,93 @@ public class MainActivity extends AppCompatActivity {
                     Bundle extras = result.getData().getExtras();
                     Bitmap img = (Bitmap) extras.get("data");
 
+                    fotinha.setRotation(90);
+
                     fotinha.setImageBitmap(img);
+
+                    analisarImagem(img, "https://scanremedio.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read"
+                            , "6c193a3b7d2747ae8fc02707a665fb7f");
+
                 }
             }
     );
 
+    private void analisarImagem(Bitmap img, String url, String chave) {
+        //Inicia o objeto para conexão com o webservice do CustomVision
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        requestQueue.start();
 
+        //Converte a imagem para ser enviada via URL
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        img.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        //imagem.recycle();
 
+        //Configura o envio ao webservice do CustomVision
+        JSONObject dados = new JSONObject();
+        try {
+            dados.put("", byteArray);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-    //código que deu errado e por enquanto acho melhor focar em algo mais confiável
-    /*private void tirarFoto(){
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "nova imagem");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "Imagem da camera");
-        img = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Configura a requisição ao webservice
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("APP_BULA", ">>>>>>>>>>>> " + response.toString());
+                //Se conseguir conectar...
+                try {
+                    //Recupera a informação da previsão
+                    JSONArray retorno = response.getJSONArray("readResult");
+                    if (retorno.length() != 0) { //Se houver contedúdo
+                        JSONObject obj1 = retorno.getJSONObject(0);
+                        obj1.getJSONArray("content");
+                        Toast.makeText(MainActivity.this, "E: " + obj1, Toast.LENGTH_SHORT).show();
+                        //Recupera o nome da Tag
+                        String nomeTag = obj1.getString("Bula");
+                        if (!nomeTag.equals(null)) { //Se a probabilidade for acima de 85%
+                            Toast.makeText(MainActivity.this, "bula: " + nomeTag, Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "Não foi encontrado o item", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, "ERRO!" + e, Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, img);
-        startActivityForResult(intent, CAPTURE_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode){
-            case PERMISSION_CODE:
-                if (grantResults.length>0 && grantResults[0] ==
-                PackageManager.PERMISSION_GRANTED) {
-                    tirarFoto();
-                } else {
-                    Toast.makeText(this, "Permissão Negada!", Toast.LENGTH_SHORT).show();
                 }
-        }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.w("APP_BULA", ">>>>>>>>>>>> " + error);
+                Log.w("APP_BULA", ">>>>>>>>>>>> " + error.getMessage());
+                Log.w("APP_BULA", ">>>>>>>>>>>> " + error.getCause());
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Ocp-Apim-Subscription-Key", chave);
+                headers.put("Content-Type", "application/octet-stream");
+                return headers;
+            }
 
+            @Override
+            public byte[] getBody() {
+                return byteArray;
+            }
+        };
+        //Executa a conexão
+        requestQueue.add(jsonObjectRequest);
     }
 
-    @SuppressLint("MissingSuperCall")
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK){
-            fotinha.setImageURI(img);
-        }
-    }*/
+    private void iniciarComponentes() {
+        fotinha = findViewById(R.id.fotoCamera);
+        btFoto = findViewById(R.id.fotobtn);
+        enviarFoto = findViewById(R.id.solicitar_resposta_btn);
+    }
+
 }
