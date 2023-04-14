3350901fd0011cb62a5efc2576e314b2601bc3e5
Guilherme Scarlati <guilhermescarlati@gmail.com>
2023-04-13 22:14:57 -0300

    13/04 -  Terminei as telas favoritos e histórico, agora só falta a de user e os tutoriais

--- app/src/main/java/com/example/usarcamera/fragments/HistoricoFragment.java
+++ app/src/main/java/com/example/usarcamera/fragments/HistoricoFragment.java
@@ -1,22 +1,39 @@
 package com.example.usarcamera.fragments;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.usarcamera.R;
+import com.example.usarcamera.classes.Historico;
+import com.example.usarcamera.databinding.FragmentHistoricoBinding;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class HistoricoFragment extends Fragment {
 
+    private FragmentHistoricoBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historico, container, false);
+        binding = FragmentHistoricoBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+
+
+
+        return root;
     }
 }
